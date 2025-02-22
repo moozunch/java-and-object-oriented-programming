f426e6f4f391378f65044b9c0a68447ee3328411
annisa :b <annisaputriaprilia12@gmail.com>
2025-02-23 00:08:20 +0700

    math java

---
+++ self-learn/conditional.java
@@ -0,0 +1,20 @@
+public class Conditional {
+    public static void main(String[] args) {
+        //Booleans represent one of two values: true or false. Yes/No. On/Off. In java only true or false.
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun); //true
+        System.out.println(isFishTasty);
+
+        int x = 10;
+        int y = 5;
+        System.out.println(x < y);
+        System.out.println(x == 10);
+
+        int myAge = 19;
+        int votingAge = 17;
+        System.out.println(myAge >= votingAge); //true, now we want if true kita mau dia melakukan action, let's use conditional statemtns.
+
+
+    }
+}
--- self-learn/math.java
+++ self-learn/math.java
@@ -1,5 +1,25 @@
 public class math {
-    public void main(String[] args) {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 10;
+
+        //1. Max find the highst value
+        System.out.println(Math.max(x, y));
+
+        //2. Min find the lowest value
+        System.out.println(Math.min(x, y));
+
+        //3. Square root
+        System.out.println(Math.sqrt(64));
+
+        //4. Absolute value - bilangan tanpa tanda positif maupun negatif, bilangan bulat
+        System.out.println(Math.abs(-4.7));
+
+        //5. Random number - returns a random number between 0.0 (inclusive), and 1.0 (exclusive)
+        System.out.println(Math.random());
+        int randomNum = (int)(Math.random() * 101); //0 to 100
+        System.out.println(randomNum);
 
     }
 }  
